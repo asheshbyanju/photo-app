2e8c80c8c1adf8f654d840805fe792233c80ce17
asheshbyanju <byanju.ashesh@gmail.com>
2018-07-09 10:46:59 +0545

    setup functionality for sign-up

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,11 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+  config.action_mailer.default_url_options = { :host => 'http://localhost:3000' }
+
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
@@ -30,7 +35,10 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
+  #config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options[:host] = 'http://localhost:3000'
   # Don't care if the mailer can't send.
+  
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
